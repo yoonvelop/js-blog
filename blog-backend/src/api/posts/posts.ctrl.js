ea0b8357a340189785ae3025b2a8e208d68a66cd
yoonvelop <yoonvelop@gmail.com>
2020-03-24 22:32:12 +0900

    feat: Request Body 검증

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 /*미들웨어 생성*/
 const {ObjectId} = mongoose.Types;
@@ -22,6 +23,24 @@ export const checkObjectId = (ctx, next)=>{
 * }
 * */
 export const write = async ctx => {
+
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증
+        title : Joi.string().required(),  //required()가 있으면 필수 항목
+        body : Joi.string().required(),
+        tags : Joi.array()
+            .items(Joi.string())
+            .required(),   // 문자열로 이루어진 배열
+    })
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if(result.error){
+        ctx.status = 400; // Bad request
+        ctx.body = result.error;
+        return;
+    }
+
     const {title, body, tags} = ctx.request.body;
     const post = new Post({
         title,
@@ -88,6 +107,22 @@ export const remove = async ctx => {
 * */
 export const update = async ctx => {
     const {id} = ctx.params;
+
+    // write에서 사용한 schema와 비슷하지만 required()가 없습니다.
+    const schema = Joi.object().keys({
+        title : Joi.string(),
+        body : Joi.string(),
+        tags : Joi.array().items(Joi.string()),
+    })
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = Joi.validate(ctx.request.body, schema);
+    if(result.error){
+        ctx.status = 400; // Bad request
+        ctx.body = result.error;
+        return;
+    }
+
     try{
         const post = await Post.findByIdAndUpdate(id,ctx.request.body,{
             new : true, //이 값을 설정하면 업데이트된 데이터를 반환합니다.
