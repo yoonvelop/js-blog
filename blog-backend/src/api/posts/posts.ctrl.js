dfa92d26b30655d68edfbbbb4c17ebf4107df900
yoonvelop <yoonvelop@gmail.com>
2020-03-24 22:17:36 +0900

    feat: ObjectId 검증

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+
+/*미들웨어 생성*/
+const {ObjectId} = mongoose.Types;
+
+export const checkObjectId = (ctx, next)=>{
+    const {id} = ctx.params;
+    if(!ObjectId.isValid(id)){
+        ctx.status = 400 // Bad Request
+        return;
+    }
+    return next();
+}
 
 /*
 * POST /api/posts
