05928b17cfa3ea892f3909a6c6f0afb6a8f39128
yoonvelop <yoonvelop@gmail.com>
2020-03-24 17:48:40 +0900

    feat: CRUD

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -3,19 +3,10 @@ import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
-const printInfo = ctx=>{
-    ctx.body={
-        method : ctx.method,
-        path : ctx.path,
-        params : ctx.params,
-    };
-}
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
 posts.patch('/:id', postsCtrl.update);
 
 export default posts;
