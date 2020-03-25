daab8f23a30ffae191eeb39ed070fa377ad8f6d8
yoonvelop <yoonvelop@gmail.com>
2020-03-25 18:01:00 +0900

    style: 회원가입 스타일링 01

--- blog-frontend/src/pages/LoginPage.js
+++ blog-frontend/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthTemplate from '../components/auth/AuthTemplate';
+import AuthForm from '../components/auth/AuthForm';
 
 const LoginPage = () => {
     return (
-        <div>
-            로그인
-        </div>
+        <AuthTemplate>
+            <AuthForm/>
+        </AuthTemplate>
     );
 };
 
