1a29d86fb43dc349ca3c54197340fe2974d0e5bc
yoonvelop <yoonvelop@gmail.com>
2020-03-27 22:08:19 +0900

    style: Editor UI

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Responsive from "../components/common/Responsive";
+import Editor from "../components/write/Editor";
 
 const WritePage = () => {
     return (
-        <div>
-            글쓰기
-        </div>
+        <Responsive>
+            <Editor/>
+        </Responsive>
     );
 };
 
