ff97414371a1be6999cab6e788de2ade4278a53b
yoonvelop <yoonvelop@gmail.com>
2020-03-30 16:01:23 +0900

    feat: 글 수정, 삭제, title

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -6,11 +6,20 @@ export const writePost = ({ title, body, tags }) =>
 
 export const readPost = id => client.get(`/api/posts/${id}`);
 
-export const listPosts = ({page, username, tag}) =>{
+export const listPosts = ({ page, username, tag }) => {
     const queryString = qs.stringify({
         page,
         username,
-        tag
+        tag,
     });
     return client.get(`/api/posts?${queryString}`);
 };
+
+export const updatePost = ({ id, title, body, tags }) =>
+    client.patch(`/api/posts/${id}`, {
+        title,
+        body,
+        tags,
+    });
+
+export const removePost = id => client.delete(`/api/posts/${id}`);
